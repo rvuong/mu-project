2619f4e402de7ac128b737af9f3b3672fd1a6425
Rémy Vuong <remy.vuong@davidson.fr>
2019-05-07 17:56:23 +0200

    Add routes examples

--- api/app.py
+++ api/app.py
@@ -6,10 +6,23 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch(['http://elasticsearch:9200'])
 app = Flask(__name__)
 
-@app.route('/api/', methods=['GET'])
+@app.route('/api/', methods=['GET', 'POST'])
 def index():
-    results = es.indices.get_alias("*")
-    return jsonify(results)
+    if request.method == 'POST':
+        return '{ "method": "POST", "result": 200 }'
+    else:
+        results = es.indices.get_alias("*")
+        return jsonify(results)
+
+# Get a single random menu
+@app.route('/api/menu/proposal', methods=['GET'])
+def menu_single_proposal():
+    return '{ "menu": { "name": "Gnocchis" } }'
+
+# Get a single menu, by id
+@app.route('/api/menu/<int:menu_id>', methods=['GET'])
+def menu_single_id(menu_id):
+    return '{ "menu": { "name": "Gnocchis (id: #%s)" } }' % menu_id
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
