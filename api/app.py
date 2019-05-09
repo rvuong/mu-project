88e562d4323e8108600e5f1c8b360c5763d90fd5
Rémy Vuong <remy.vuong@davidson.fr>
2019-05-09 15:12:30 +0200

    Adds status codes to the API

--- api/app.py
+++ api/app.py
@@ -9,20 +9,20 @@ app = Flask(__name__)
 @app.route('/api/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return '{ "method": "POST", "result": 200 }'
+        return '{ "method": "POST" }', 201
     else:
         results = es.indices.get_alias("*")
-        return jsonify(results)
+        return jsonify(results), 200
 
 # Get a single random menu
 @app.route('/api/menu/proposal', methods=['GET'])
 def menu_single_proposal():
-    return '{ "menu": { "name": "Gnocchis" } }'
+    return '{ "menu": { "name": "Gnocchis" } }', 200
 
 # Get a single menu, by id
 @app.route('/api/menu/<int:menu_id>', methods=['GET'])
 def menu_single_id(menu_id):
-    return '{ "menu": { "name": "Gnocchis (id: #%s)" } }' % menu_id
+    return '{ "menu": { "name": "Gnocchis (id: #%s)" } }' % menu_id, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
